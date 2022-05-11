7bfb7b799c8967a25eceada2c389d4665a9b3899
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2022-05-10 19:20:31 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,8 +15,26 @@ public class Exercise1 {
 	}
 
 	public String convertirBaseGabriel(int a) {
-
-		// TO DO
+               int d,i=0,exp=0,num=0;
+		boolean gabriel;
+	         gabriel = true;
+	          while (a!= 0) {
+	                     d= a % 10;
+	                   if (d!= 0 && d != 1&!(d==2&& i==0)) { 
+	                    gabriel = false;
+	                    break;
+	                   }
+	                    if(d==1) {
+	                  	 i=1;
+	                   } 
+	                     num=num+d*((int)Math.pow(2,exp+1)-1);
+	        			 exp++;
+	        			 a=a/10;                             
+	           }
+	        if(gabriel==true) {
+	       return String.valueOf(num);
+	       }else
 		return "El numero proporcionado no esta en base Gabriel.";
+		
 	}
 }
