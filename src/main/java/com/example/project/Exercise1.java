b41a9419e0c4524dc3cbc396a962f87be018a574
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2022-05-10 19:56:48 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -4,35 +4,37 @@ import java.util.Scanner;
 
 public class Exercise1 {
 	public static void main(final String[] args) {
+public static void main(final String[] args) {
 
 		Exercise1 obj = new Exercise1();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int n = sc.nextInt();
-			System.out.println(obj.convertirBaseGabriel(n));			
+			System.out.println(obj.convertirBaseGabriel(n));
 		}
 	}
+
 	public String convertirBaseGabriel(int a) {
-               int d,i=0,exp=0,num=0;
+		int d, i = 0, exp = 0, num = 0;
 		boolean gabriel;
-	         gabriel = true;
-	          while (a!= 0) {
-	                     d= a % 10;
-	                   if (d!= 0 && d != 1&!(d==2&& i==0)) { 
-	                    gabriel = false;
-	                    break;
-	                   }
-	                    if(d==1) {
-	                  	 i=1;
-	                   } 
-	                     num=num+d*((int)Math.pow(2,exp+1)-1);
-	        			 exp++;
-	        			 a=a/10;                             
-	           }
-	        if(gabriel==true) {
-	       return String.valueOf(num);
-	       }else
-		return "El numero proporcionado no esta en base Gabriel.";
-		
+		gabriel = true;
+		while (a != 0) {
+			d = a % 10;
+			if (d != 0 && d != 1 & !(d == 2 && i == 0)) {
+				gabriel = false;
+				break;
+			}
+			if (d == 1) {
+				i = 1;
+			}
+			num = num + d * ((int) Math.pow(2, exp + 1) - 1);
+			exp++;
+			a = a / 10;
+		}
+		if (gabriel == true) {
+			return String.valueOf(num);
+		} else
+			return "El numero proporcionado no esta en base Gabriel.";
+
 	}
 }
