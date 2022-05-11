337c6c4f6e924543769ffb4d759cb62cc4bb1ae8
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2022-05-10 20:06:49 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -3,8 +3,8 @@ package com.example.project;
 import java.util.Scanner;
 
 public class Exercise1 {
+
 	public static void main(final String[] args) {
-public static void main(final String[] args) {
 
 		Exercise1 obj = new Exercise1();
 		Scanner sc = new Scanner(System.in);
